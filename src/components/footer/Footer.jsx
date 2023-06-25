00daff3e26328866544acc6449916abe4d9e0f91
Raman Davidovich <raman.davidovich@gmail.com>
2023-06-25 21:07:46 +0300

    feat: addde Reveal animation

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,23 +3,28 @@ import "./footer.scss";
 import { Tooltip } from "react-tooltip";
 import footerData from "./constants";
 import { isDesktop } from "react-device-detect";
+import Reveal from "../shared/Reveal/Reveal";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="/#" className="footer__logo">
-        Raman Davidovich
-      </a>
+      <Reveal width="100%">
+        <a href="/#" className="footer__logo">
+          Raman Davidovich
+        </a>
+      </Reveal>
 
       <nav>
         <ul className="permalinks">
           {footerData.info.map(({ href, content }) => {
             return (
-              <li key={content}>
-                <a href={href} aria-label={`Go to ${content} section.`}>
-                  {content}
-                </a>
-              </li>
+              <Reveal key={content}>
+                <li>
+                  <a href={href} aria-label={`Go to ${content} section.`}>
+                    {content}
+                  </a>
+                </li>
+              </Reveal>
             );
           })}
         </ul>
@@ -28,28 +33,32 @@ const Footer = () => {
       <ul className="footer__socials">
         {footerData.socials.map(({ href, dataTooltipContent, icon }) => {
           return (
-            <li key={dataTooltipContent}>
-              <a
-                href={href}
-                aria-label={`Visit Raman's ${dataTooltipContent} account.`}
-                target="_blank"
-                rel="noreferrer"
-                className="tooltip_footer"
-                data-tooltip-content={dataTooltipContent}
-                data-tooltip-delay-show={500}
-                data-tooltip-variant="dark"
-                data-tooltip-hidden={!isDesktop}
-              >
-                {icon}
-              </a>
-            </li>
+            <Reveal key={dataTooltipContent}>
+              <li>
+                <a
+                  href={href}
+                  aria-label={`Visit Raman's ${dataTooltipContent} account.`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="tooltip_footer"
+                  data-tooltip-content={dataTooltipContent}
+                  data-tooltip-delay-show={500}
+                  data-tooltip-variant="dark"
+                  data-tooltip-hidden={!isDesktop}
+                >
+                  {icon}
+                </a>
+              </li>
+            </Reveal>
           );
         })}
       </ul>
       <Tooltip anchorSelect=".tooltip_footer" />
 
       <div className="footer__copyright">
-        <small>{new Date().getFullYear()} &copy; All rights reserved.</small>
+        <Reveal width="100%">
+          <small>{new Date().getFullYear()} &copy; All rights reserved.</small>
+        </Reveal>
       </div>
     </footer>
   );
